f597e1199b6e113de6ec72552130a16b29e5d9fd
Franziska Hinkelmann <franzih@chromium.org>
2019-03-19 12:08:09 -0400

    Use async/await for GitHub

--- githubStats.js
+++ githubStats.js
@@ -1,16 +1,20 @@
 const Octokit = require('@octokit/rest')
-const octokit = new Octokit ()
+const octokit = new Octokit()
 
-// Compare: https://developer.github.com/v3/repos/#list-organization-repositories
-octokit.repos.listForOrg({
-  org: 'octokit',
-  type: 'public'
-}).then(({ data, status, headers }) => {
-  console.log(`Found ${data.length} repos:`);
-  for(const repo of data) {
-    console.log(repo.html_url);
-    console.log(repo.full_name);
-    console.log();
+
+const listOrgRepos = async () => {
+
+  // Compare: https://developer.github.com/v3/repos/#list-organization-repositories
+  const { data } = await octokit.repos.listForOrg({
+    org: 'GoogleCloudPlatform',
+    type: 'public'
+  });
+  console.log(`Found ${data.length} repos`);
+  for (const repo of data) {
+    // console.log(repo.html_url);
+    // console.log(repo.full_name);
+    // console.log();
   }
-  // handle data
-})
+}
+
+listOrgRepos();
