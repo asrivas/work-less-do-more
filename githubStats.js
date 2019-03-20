6560e5d21a4588786580f53972180603323f5087
Franziska Hinkelmann <franzih@chromium.org>
2019-03-20 08:48:51 -0400

    Get number of clones

--- githubStats.js
+++ githubStats.js
@@ -1,20 +1,29 @@
-const Octokit = require('@octokit/rest')
-const octokit = new Octokit()
+const Octokit = require('@octokit/rest');
+const octokit = new Octokit({auth: 'token d7106f7559568b2aa57fba67828052f473db29a9'});
 
 
 const listOrgRepos = async () => {
-
   // Compare: https://developer.github.com/v3/repos/#list-organization-repositories
-  const { data } = await octokit.repos.listForOrg({
+  const options = await octokit.repos.listForOrg.endpoint.merge({
     org: 'GoogleCloudPlatform',
     type: 'public'
   });
-  console.log(`Found ${data.length} repos`);
-  for (const repo of data) {
+  const repos = await octokit.paginate(options);
+  console.log(`Found ${repos.length} repos`);
+  for (const repo of repos) {
     // console.log(repo.html_url);
     // console.log(repo.full_name);
     // console.log();
   }
 }
 
-listOrgRepos();
+const numberOfClones = async () => {
+  // https://octokit.github.io/rest.js/#api-Repos-getClones
+  const {data} = await octokit.repos.getClones({
+    owner: 'fhinkel', repo: 'twitch'
+  });
+  console.log(data.count);
+}
+
+// listOrgRepos();
+numberOfClones();
