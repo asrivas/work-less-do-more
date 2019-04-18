bbca80755b05e9427a633f52e6447e9bde7e1ae9
asrivas <asrivastava274@gmail.com>
2019-04-18 10:40:03 -0400

    Start Cloud Function to work with Sheets

---
+++ functions/index.js
@@ -0,0 +1,33 @@
+const { google } = require('googleapis');
+
+/**
+ * Updates data into the chart from Github and mails a chart.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.githubChart = (req, res) => {
+    let message = req.query.message || req.body.message || 'Hello World!';
+
+    let auth = await google.auth.getClient({
+        scopes: ['https://www.googleapis.com/auth/spreadsheets']
+    });
+    const sheets = google.sheets({ version: 'v4', auth });
+    await createSpreadsheet(sheets, "Sheet from function");
+    res.status(200).send(message);
+};
+
+/**
+ * Creates a spreadsheet with the given title.
+ */
+async function createSpreadsheet(sheets, title) {
+    const resource = {
+        properties: {
+            title,
+        }
+    }
+    const { data } = await sheets.spreadsheets.create({ resource });
+
+    console.log(`Created new spreadsheet with ID: ${data.spreadsheetId}`);
+    return data.spreadsheetId;
+}
