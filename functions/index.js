9fd7d1503a4ba480431d542244fc1a6dead4b956
Franziska Hinkelmann <franzih@chromium.org>
2019-04-18 10:49:18 -0400

    Show the spreadsheet id to the user

--- functions/index.js
+++ functions/index.js
@@ -7,28 +7,32 @@ const { google } = require('googleapis');
  * @param {!express:Response} res HTTP response context.
  */
 exports.githubChart = (req, res) => {
-    let message = req.query.message || req.body.message || 'Hello World!';
-    setUp().then(()=> res.status(200).send(message));
+  setUp().then((id) => res.status(200).send(id));
 };
 
 async function setUp() {
-    let auth = await google.auth.getClient({
-        scopes: ['https://www.googleapis.com/auth/spreadsheets']
-    });
-    const sheets = google.sheets({ version: 'v4', auth });
-    await createSpreadsheet(sheets, "Sheet from function");
-} 
+  let auth = await google.auth.getClient({
+    scopes: ['https://www.googleapis.com/auth/spreadsheets']
+  });
+  const sheets = google.sheets({ version: 'v4', auth });
+  let id = await createSpreadsheet(sheets, "Sheet from function");
+  return id;
+}
 
 /**
  * Creates a spreadsheet with the given title.
  */
 async function createSpreadsheet(sheets, title) {
-    const resource = {
-        properties: {
-            title,
-        }
+  const resource = {
+    properties: {
+      title,
     }
+  }
+  try {
     const { data } = await sheets.spreadsheets.create({ resource });
-    console.log(`Created new spreadsheet with ID: ${data.spreadsheetId}`);
-    return data.spreadsheetId;
+  } catch (err) {
+    console.log(`error: ${err}`);
+  }
+  console.log(`Created new spreadsheet with ID: ${data.spreadsheetId}`);
+  return data.spreadsheetId;
 }
