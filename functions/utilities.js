2f48bf6226a04888c8655f419328ca058432ece1
asrivas <asrivastava274@gmail.com>
2019-04-20 16:06:17 -0400

    Format form AND github response dates with last row index

--- functions/utilities.js
+++ functions/utilities.js
@@ -4,214 +4,262 @@ const githubUtilities = require('./githubUtilities');
 const fs = require('fs').promises;
 
 addUser = async (drive, id, emailAddress) => {
-  try {
-    let { data } = await drive.permissions.create({
-      fileId: id,
-      type: 'user',
-      resource: {
-        type: 'user',
-        // TODO(asrivast): Lower this permission level. 
-        role: 'writer',
-        emailAddress,
-        transferOwnership: false,
-      },
-    });
-    console.log(`Permission Id: ${data.id}`);
-  } catch (err) {
-    console.error(`Failed sharing with ${emailAddress}`);
-    console.error(err);
-  }
+    try {
+        let { data } = await drive.permissions.create({
+            fileId: id,
+            type: 'user',
+            resource: {
+                type: 'user',
+                // TODO(asrivast): Lower this permission level. 
+                role: 'writer',
+                emailAddress,
+                transferOwnership: false,
+            },
+        });
+        console.log(`Permission Id: ${data.id}`);
+    } catch (err) {
+        console.error(`Failed sharing with ${emailAddress}`);
+        console.error(err);
+    }
 }
 
 appendCloneData = async (sheets, spreadsheetId, cloneData) => {
-  const range = 'Sheet1!A2';
-  const valueInputOption = 'USER_ENTERED';
-  const values = [];
-  for (const entry of cloneData) {
-      let date = entry.timestamp.split('T')[0];
-    values.push([date, , entry.uniques]);
-  }
-
-  const response = await sheets.spreadsheets.values.append({
-    spreadsheetId,
-    range,
-    valueInputOption,
-    resource: {
-      values
+    const range = 'Sheet1!A2';
+    const valueInputOption = 'USER_ENTERED';
+    const values = [];
+    for (const entry of cloneData) {
+        let date = entry.timestamp.split('T')[0];
+        values.push([date, , entry.uniques]);
     }
-  });
-  const updatedRange = response.data.updates.updatedRange;
-  console.log(`Appending data: ${response.status}`);
-  return Number(updatedRange.split(':')[1].split('C')[1]);
+
+    const response = await sheets.spreadsheets.values.append({
+        spreadsheetId,
+        range,
+        valueInputOption,
+        resource: {
+            values
+        }
+    });
+    const updatedRange = response.data.updates.updatedRange;
+    console.log(`Appending data: ${response.status}`);
+    return Number(updatedRange.split(':')[1].split('C')[1]);
 }
 
 exports.setUp = async (title) => {
-  let auth = await google.auth.getClient({
-    scopes: ['https://www.googleapis.com/auth/drive.file']
-  });
-  const sheets = google.sheets({ version: 'v4', auth });
-  const drive = google.drive({ version: 'v3', auth });
+    let auth = await google.auth.getClient({
+        scopes: ['https://www.googleapis.com/auth/drive.file']
+    });
+    const sheets = google.sheets({ version: 'v4', auth });
+    const drive = google.drive({ version: 'v3', auth });
 
-  let id = await idOfSheet(drive, title);
-  if (!id) {
-    id = await createSpreadsheet(sheets, title);
-  }
+    let id = await idOfSheet(drive, title);
+    if (!id) {
+        id = await createSpreadsheet(sheets, title);
+    }
 
-  const token = (await fs.readFile('./githubToken.json')).toString().trim();
-  const octokit = new Octokit({ auth: `token ${token}` });
-  console.log('Fetching github data');
+    const token = (await fs.readFile('./githubToken.json')).toString().trim();
+    const octokit = new Octokit({ auth: `token ${token}` });
+    console.log('Fetching github data');
 
-  try {
-    const cloneData = await githubUtilities.numberOfClones(octokit,
-      'GoogleCloudPlatform', 'nodejs-getting-started');
-    const lastRow = await appendCloneData(sheets, id, cloneData.clones).catch(err => console.error(err));
-    console.log(lastRow);
-    await createChart(sheets, id, lastRow);
-  } catch (err) {
-    console.error(`Error: ${err}`);
-  }
+    try {
+        const cloneData = await githubUtilities.numberOfClones(octokit,
+            'GoogleCloudPlatform', 'nodejs-getting-started');
+        const lastRowIndex = await appendCloneData(sheets, id, cloneData.clones).catch(err => console.error(err));
+        await updateCellFormatToDate(sheets, id, lastRowIndex);
+        await createChart(sheets, id, lastRowIndex);
+    } catch (err) {
+        console.error(`Error: ${err}`);
+    }
 
 
-  //  TODO(asrivast): Use IAM, read email from request.  
-  await addUser(drive, id, 'gsuite.demos@gmail.com');
-  await addUser(drive, id, 'fhinkel.demo@gmail.com');
+    //  TODO(asrivast): Use IAM, read email from request.  
+    await addUser(drive, id, 'gsuite.demos@gmail.com');
+    await addUser(drive, id, 'fhinkel.demo@gmail.com');
 
-  return id;
+    return id;
 }
 
 /**
  * Creates a spreadsheet with the given title.
  */
 async function createSpreadsheet(sheets, title) {
-  const resource = {
-    properties: {
-      title,
+    const resource = {
+        properties: {
+            title,
+        }
     }
-  }
-  try {
-    const { data } = await sheets.spreadsheets.create({ resource });
-    console.log(`Created new spreadsheet with ID: ${data.spreadsheetId} `);
-    return data.spreadsheetId;
-  } catch (err) {
-    console.log(`error: ${err} `);
-    return err;
-  }
+    try {
+        const { data } = await sheets.spreadsheets.create({ resource });
+        console.log(`Created new spreadsheet with ID: ${data.spreadsheetId} `);
+        return data.spreadsheetId;
+    } catch (err) {
+        console.log(`error: ${err} `);
+        return err;
+    }
 }
 
 
 const idOfSheet = async (drive, title) => {
-  try {
-    let { data } = await drive.files.list();
-    for (const file of data.files) {
-      if (file.name === title) {
-        console.log(`Using existing file: ${file.id}`);
-        return file.id;
-      }
+    try {
+        let { data } = await drive.files.list();
+        for (const file of data.files) {
+            if (file.name === title) {
+                console.log(`Using existing file: ${file.id}`);
+                return file.id;
+            }
+        }
+    } catch (err) {
+        console.log('Listing files failed.')
+        console.log(err);
     }
-  } catch (err) {
-    console.log('Listing files failed.')
-    console.log(err);
-  }
 }
 
-async function getSheetId(sheets, spreadsheetId) {
-  const { data } = await sheets.spreadsheets.get({
-    spreadsheetId,
-  });
-  let { properties } = data.sheets[0]
-  const sheetId = properties.sheetId;
-  return sheetId;
+async function getSheetId(sheets, spreadsheetId, index) {
+    const { data } = await sheets.spreadsheets.get({
+        spreadsheetId,
+    });
+    let { properties } = data.sheets[index]
+    const sheetId = properties.sheetId;
+    return sheetId;
 }
 
 async function createChart(sheets, spreadsheetId, endRowIndex) {
-  const sheetId = await getSheetId(sheets, spreadsheetId);
-  const requests = [{
-    addChart: {
-      "chart": {
-        "spec": {
-          "title": "Best data ever",
-          "basicChart": {
-            "chartType": "LINE",
-            "legendPosition": "BOTTOM_LEGEND",
-            "axis": [
-              {
-                "position": "BOTTOM_AXIS",
-                "title": "Date"
-              },
-              {
-                "position": "LEFT_AXIS",
-                "title": "Number of Clones"
-              }
-            ],
-            // "domains": [
-            //   {
-            //     "domain": {
-            //       "sourceRange": {
-            //         "sources": [
-            //           {
-            //             "sheetId": sheetId,
-            //             "startRowIndex": 0,
-            //             "endRowIndex": 7,
-            //             "startColumnIndex": 0,
-            //             "endColumnIndex": 1
-            //           }
-            //         ]
-            //       }
-            //     }
-            //   }
-            // ],
-            "series": [
-              {
-                "series": {
-                  "sourceRange": {
-                    "sources": [
-                      {
-                        "sheetId": sheetId,
-                        "startRowIndex": 0,
-                        "endRowIndex": endRowIndex,
-                        "startColumnIndex": 0,
-                        "endColumnIndex": 1
-                      }
-                    ]
-                  }
+    const sheetId = await getSheetId(sheets, spreadsheetId, 1);
+    const requests = [{
+        addChart: {
+            "chart": {
+                "spec": {
+                    "title": "Best data ever",
+                    "basicChart": {
+                        "chartType": "LINE",
+                        "legendPosition": "BOTTOM_LEGEND",
+                        "axis": [
+                            {
+                                "position": "BOTTOM_AXIS",
+                                "title": "Date"
+                            },
+                            {
+                                "position": "LEFT_AXIS",
+                                "title": "Number of Clones"
+                            }
+                        ],
+                        // "domains": [
+                        //   {
+                        //     "domain": {
+                        //       "sourceRange": {
+                        //         "sources": [
+                        //           {
+                        //             "sheetId": sheetId,
+                        //             "startRowIndex": 0,
+                        //             "endRowIndex": 7,
+                        //             "startColumnIndex": 0,
+                        //             "endColumnIndex": 1
+                        //           }
+                        //         ]
+                        //       }
+                        //     }
+                        //   }
+                        // ],
+                        "series": [
+                            {
+                                "series": {
+                                    "sourceRange": {
+                                        "sources": [
+                                            {
+                                                "sheetId": sheetId,
+                                                "startRowIndex": 0,
+                                                "endRowIndex": endRowIndex,
+                                                "startColumnIndex": 0,
+                                                "endColumnIndex": 1
+                                            }
+                                        ]
+                                    }
+                                },
+                                "targetAxis": "LEFT_AXIS"
+                            },
+                            {
+                                "series": {
+                                    "sourceRange": {
+                                        "sources": [
+                                            {
+                                                "sheetId": sheetId,
+                                                "startRowIndex": 0,
+                                                "endRowIndex": endRowIndex,
+                                                "startColumnIndex": 2,
+                                                "endColumnIndex": 3
+                                            }
+                                        ]
+                                    }
+                                },
+                                "targetAxis": "LEFT_AXIS"
+                            },
+                        ],
+                        "headerCount": 1
+                    }
                 },
-                "targetAxis": "LEFT_AXIS"
-              },
-              {
-                "series": {
-                  "sourceRange": {
-                    "sources": [
-                      {
-                        "sheetId": sheetId,
-                        "startRowIndex": 0,
-                        "endRowIndex": endRowIndex,
-                        "startColumnIndex": 2,
-                        "endColumnIndex": 3
-                      }
-                    ]
-                  }
-                },
-                "targetAxis": "LEFT_AXIS"
-              },
-            ],
-            "headerCount": 1
-          }
-        },
-        "position": {
-          "newSheet": true
+                "position": {
+                    "newSheet": true
+                }
+            }
         }
-      }
+    }];
+
+    const resource = {
+        requests,
     }
-  }];
 
-  const resource = {
-    requests,
-  }
+    const response = await sheets.spreadsheets.batchUpdate({
+        spreadsheetId,
+        resource,
+    })
+    console.log('Chart created with Id: ' + response.data.replies[0].addChart.chart.chartId);
+}
 
-  const response = await sheets.spreadsheets.batchUpdate({
-    spreadsheetId,
-    resource,
-  })
-  console.log('Chart created with Id: ' + response.data.replies[0].addChart.chart.chartId);
+async function updateCellFormatToDate(sheets, spreadsheetId, githubLastRowIndex) {
+    const formResponsesSheetId = await getSheetId(sheets, spreadsheetId, 0);
+    const githubSheetId = await getSheetId(sheets, spreadsheetId, 1);
+
+    const requests = [{
+        repeatCell: {
+            range: {
+                sheetId: formResponsesSheetId,
+                startRowIndex: 0,
+                endRowIndex: githubLastRowIndex,
+            },
+            cell: {
+                userEnteredFormat: {
+                    numberFormat: {
+                        type: 'DATE'
+                    }
+                }
+            },
+            fields: 'userEnteredFormat(numberFormat)'
+        },
+        repeatCell: {
+            range: {
+                sheetId: githubSheetId,
+                startRowIndex: 0,
+                endRowIndex: githubLastRowIndex,
+            },
+            cell: {
+                userEnteredFormat: {
+                    numberFormat: {
+                        type: 'DATE'
+                    }
+                }
+            },
+            fields: 'userEnteredFormat(numberFormat)'
+        },
+
+    }];
+    const resource = {
+        requests,
+    }
+
+    const response = await sheets.spreadsheets.batchUpdate({
+        spreadsheetId,
+        resource,
+    });
+    console.log(response);
 }
 
