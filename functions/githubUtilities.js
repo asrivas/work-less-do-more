3867d8c8d611ab5349e79e0da4328eb1c10ffcd6
Franziska Hinkelmann <franzih@chromium.org>
2019-04-22 10:35:58 -0400

    Get number of open issues without PRs

--- functions/githubUtilities.js
+++ functions/githubUtilities.js
@@ -2,40 +2,37 @@
  * Returns the number of clones for repo the last 2 weeks.
  */
 exports.numberOfClones = async (octokit, owner, repo) => {
-    console.log(`Fetching number of clones for repo: ${repo}`);
-    try {
-        // https://octokit.github.io/rest.js/#api-Repos-getClones
-        let { data } = await octokit.repos.getClones({
-            owner: owner, repo: repo,
-        });
-        console.log(`Clones: ${data}`);
-        console.log(data);
-        return data;
-    } catch (err) {
-        console.error('Could not get Github clones');
-        console.error(err);
-    }
+  console.log(`Fetching number of clones for repo: ${repo}`);
+  try {
+    // https://octokit.github.io/rest.js/#api-Repos-getClones
+    let { data } = await octokit.repos.getClones({
+      owner: owner, repo: repo,
+    });
+    console.log(`Clones: ${data}`);
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.error('Could not get Github clones');
+    console.error(err);
+  }
 }
 
 /**
  * Returns the number of issues for repo.
  */
 exports.numberOfIssues = async (octokit, owner, repo) => {
-    console.log(`Fetching number of issues for repo: ${repo}`);
-    try {
-        const options = await octokit.issues.listForRepo.endpoint.merge({
-            owner,
-            repo
-        });
-        const issues = await octokit.paginate(options);
-        console.log(`Found ${issues.length} issues`);
-        for (const issue of issues) {
-             console.log(issue.title);
-            // console.log(repo.full_name);
-            // console.log();
-        }
-    } catch (err) {
-        console.error('Could not get Github issues');
-        console.error(err);
-    }
+  console.log(`Fetching number of issues for repo: ${repo}`);
+  try {
+    const options = await octokit.issues.listForRepo.endpoint.merge({
+      owner,
+      repo
+    });
+    const issues = await octokit.paginate(options);
+    console.log(`Found ${issues.length} issues`);
+    let prs = issues.filter(entry => entry.pull_request);
+    return issues.length - prs.length;
+  } catch (err) {
+    console.error('Could not get Github issues');
+    console.error(err);
+  }
 }
--- functions/utilities.js
+++ functions/utilities.js
@@ -4,252 +4,253 @@ const githubUtilities = require('./githubUtilities');
 const fs = require('fs').promises;
 
 addUser = async (drive, id, emailAddress) => {
-    try {
-        let { data } = await drive.permissions.create({
-            fileId: id,
-            type: 'user',
-            resource: {
-                type: 'user',
-                // TODO(asrivast): Lower this permission level. 
-                role: 'writer',
-                emailAddress,
-                transferOwnership: false,
-            },
-        });
-        console.log(`Permission Id: ${data.id}`);
-    } catch (err) {
-        console.error(`Failed sharing with ${emailAddress}`);
-        console.error(err);
-    }
+  try {
+    let { data } = await drive.permissions.create({
+      fileId: id,
+      type: 'user',
+      resource: {
+        type: 'user',
+        // TODO(asrivast): Lower this permission level. 
+        role: 'writer',
+        emailAddress,
+        transferOwnership: false,
+      },
+    });
+    console.log(`Permission Id: ${data.id}`);
+  } catch (err) {
+    console.error(`Failed sharing with ${emailAddress}`);
+    console.error(err);
+  }
 }
 
 appendCloneData = async (sheets, spreadsheetId, cloneData) => {
-    const range = 'Sheet1!A2';
-    const valueInputOption = 'USER_ENTERED';
-    const values = [];
-    for (const entry of cloneData) {
-        let date = entry.timestamp.split('T')[0];
-        values.push([date, , entry.uniques]);
-    }
+  const range = 'Sheet1!A2';
+  const valueInputOption = 'USER_ENTERED';
+  const values = [];
+  for (const entry of cloneData) {
+    let date = entry.timestamp.split('T')[0];
+    values.push([date, , entry.uniques]);
+  }
 
-    const response = await sheets.spreadsheets.values.append({
-        spreadsheetId,
-        range,
-        valueInputOption,
-        resource: {
-            values
-        }
-    });
-    const updatedRange = response.data.updates.updatedRange;
-    console.log(`Appending data: ${response.status}`);
-    return Number(updatedRange.split(':')[1].split('C')[1]);
+  const response = await sheets.spreadsheets.values.append({
+    spreadsheetId,
+    range,
+    valueInputOption,
+    resource: {
+      values
+    }
+  });
+  const updatedRange = response.data.updates.updatedRange;
+  console.log(`Appending data: ${response.status}`);
+  return Number(updatedRange.split(':')[1].split('C')[1]);
 }
 
 exports.setUp = async (title) => {
-    let auth = await google.auth.getClient({
-        scopes: ['https://www.googleapis.com/auth/drive.file']
-    });
-    const sheets = google.sheets({ version: 'v4', auth });
-    const drive = google.drive({ version: 'v3', auth });
+  // let auth = await google.auth.getClient({
+  //   scopes: ['https://www.googleapis.com/auth/drive.file']
+  // });
+  // const sheets = google.sheets({ version: 'v4', auth });
+  // const drive = google.drive({ version: 'v3', auth });
 
-    let id = await idOfSheet(drive, title);
-    if (!id) {
-        id = await createSpreadsheet(sheets, title);
-    }
+  // let id = await idOfSheet(drive, title);
+  // if (!id) {
+  //   id = await createSpreadsheet(sheets, title);
+  // }
 
-    const token = (await fs.readFile('./githubToken.json')).toString().trim();
-    const octokit = new Octokit({ auth: `token ${token}` });
-    console.log('Fetching github data');
+  const token = (await fs.readFile('./githubToken.json')).toString().trim();
+  const octokit = new Octokit({ auth: `token ${token}` });
+  console.log('Fetching github data');
 
-    try {
-        const issues = await githubUtilities.numberOfIssues(octokit,
-            'nodejs', 'node');
-            return;
+  try {
+    const numberOfIssues = await githubUtilities.numberOfIssues(octokit,
+      'nodejs', 'node');
+    console.log(`Number of open issues: ${numberOfIssues}`);
+    return;
 
-        const cloneData = await githubUtilities.numberOfClones(octokit,
-            'GoogleCloudPlatform', 'nodejs-getting-started');
-        const lastRowIndex = await appendCloneData(sheets, id, cloneData.clones).catch(err => console.error(err));
-        await updateCellFormatToDate(sheets, id, lastRowIndex);
-        await createChart(sheets, id, lastRowIndex);
-    } catch (err) {
-        console.error(`Error: ${err}`);
-    }
+    const cloneData = await githubUtilities.numberOfClones(octokit,
+      'GoogleCloudPlatform', 'nodejs-getting-started');
+    const lastRowIndex = await appendCloneData(sheets, id, cloneData.clones).catch(err => console.error(err));
+    await updateCellFormatToDate(sheets, id, lastRowIndex);
+    await createChart(sheets, id, lastRowIndex);
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
 
-    //  TODO(asrivast): Use IAM, read email from request.  
-    await addUser(drive, id, 'gsuite.demos@gmail.com');
-    await addUser(drive, id, 'fhinkel.demo@gmail.com');
+  //  TODO(asrivast): Use IAM, read email from request.  
+  await addUser(drive, id, 'gsuite.demos@gmail.com');
+  await addUser(drive, id, 'fhinkel.demo@gmail.com');
 
-    return id;
+  return id;
 }
 
 /**
  * Creates a spreadsheet with the given title.
  */
 async function createSpreadsheet(sheets, title) {
-    const resource = {
-        properties: {
-            title,
-        }
+  const resource = {
+    properties: {
+      title,
     }
-    try {
-        const { data } = await sheets.spreadsheets.create({ resource });
-        console.log(`Created new spreadsheet with ID: ${data.spreadsheetId} `);
-        return data.spreadsheetId;
-    } catch (err) {
-        console.log(`error: ${err} `);
-        return err;
-    }
+  }
+  try {
+    const { data } = await sheets.spreadsheets.create({ resource });
+    console.log(`Created new spreadsheet with ID: ${data.spreadsheetId} `);
+    return data.spreadsheetId;
+  } catch (err) {
+    console.log(`error: ${err} `);
+    return err;
+  }
 }
 
 
 const idOfSheet = async (drive, title) => {
-    try {
-        let { data } = await drive.files.list();
-        for (const file of data.files) {
-            if (file.name === title) {
-                console.log(`Using existing file: ${file.id}`);
-                return file.id;
-            }
-        }
-    } catch (err) {
-        console.log('Listing files failed.')
-        console.log(err);
+  try {
+    let { data } = await drive.files.list();
+    for (const file of data.files) {
+      if (file.name === title) {
+        console.log(`Using existing file: ${file.id}`);
+        return file.id;
+      }
     }
+  } catch (err) {
+    console.log('Listing files failed.')
+    console.log(err);
+  }
 }
 
 async function getSheetId(sheets, spreadsheetId, index) {
-    const { data } = await sheets.spreadsheets.get({
-        spreadsheetId,
-    });
-    let { properties } = data.sheets[index]
-    const sheetId = properties.sheetId;
-    return sheetId;
+  const { data } = await sheets.spreadsheets.get({
+    spreadsheetId,
+  });
+  let { properties } = data.sheets[index]
+  const sheetId = properties.sheetId;
+  return sheetId;
 }
 
 async function createChart(sheets, spreadsheetId, endRowIndex) {
-    const sheetId = await getSheetId(sheets, spreadsheetId, 1);
-    const requests = [{
-        addChart: {
-            "chart": {
-                "spec": {
-                    "title": "Best data ever",
-                    "basicChart": {
-                        "chartType": "LINE",
-                        "legendPosition": "BOTTOM_LEGEND",
-                        "axis": [
-                            {
-                                "position": "BOTTOM_AXIS",
-                                "title": "Date"
-                            },
-                            {
-                                "position": "LEFT_AXIS",
-                                "title": "Number of Clones"
-                            }
-                        ],
-                        "series": [
-                            {
-                                "series": {
-                                    "sourceRange": {
-                                        "sources": [
-                                            {
-                                                "sheetId": sheetId,
-                                                "startRowIndex": 0,
-                                                "endRowIndex": endRowIndex,
-                                                "startColumnIndex": 0,
-                                                "endColumnIndex": 1
-                                            }
-                                        ]
-                                    }
-                                },
-                                "targetAxis": "LEFT_AXIS"
-                            },
-                            {
-                                "series": {
-                                    "sourceRange": {
-                                        "sources": [
-                                            {
-                                                "sheetId": sheetId,
-                                                "startRowIndex": 0,
-                                                "endRowIndex": endRowIndex,
-                                                "startColumnIndex": 2,
-                                                "endColumnIndex": 3
-                                            }
-                                        ]
-                                    }
-                                },
-                                "targetAxis": "LEFT_AXIS"
-                            },
-                        ],
-                        "headerCount": 1
-                    }
+  const sheetId = await getSheetId(sheets, spreadsheetId, 1);
+  const requests = [{
+    addChart: {
+      "chart": {
+        "spec": {
+          "title": "Best data ever",
+          "basicChart": {
+            "chartType": "LINE",
+            "legendPosition": "BOTTOM_LEGEND",
+            "axis": [
+              {
+                "position": "BOTTOM_AXIS",
+                "title": "Date"
+              },
+              {
+                "position": "LEFT_AXIS",
+                "title": "Number of Clones"
+              }
+            ],
+            "series": [
+              {
+                "series": {
+                  "sourceRange": {
+                    "sources": [
+                      {
+                        "sheetId": sheetId,
+                        "startRowIndex": 0,
+                        "endRowIndex": endRowIndex,
+                        "startColumnIndex": 0,
+                        "endColumnIndex": 1
+                      }
+                    ]
+                  }
                 },
-                "position": {
-                    "newSheet": true
-                }
-            }
+                "targetAxis": "LEFT_AXIS"
+              },
+              {
+                "series": {
+                  "sourceRange": {
+                    "sources": [
+                      {
+                        "sheetId": sheetId,
+                        "startRowIndex": 0,
+                        "endRowIndex": endRowIndex,
+                        "startColumnIndex": 2,
+                        "endColumnIndex": 3
+                      }
+                    ]
+                  }
+                },
+                "targetAxis": "LEFT_AXIS"
+              },
+            ],
+            "headerCount": 1
+          }
+        },
+        "position": {
+          "newSheet": true
         }
-    }];
-
-    const resource = {
-        requests,
+      }
     }
+  }];
 
-    const response = await sheets.spreadsheets.batchUpdate({
-        spreadsheetId,
-        resource,
-    })
-    console.log('Chart created with Id: ' + response.data.replies[0].addChart.chart.chartId);
+  const resource = {
+    requests,
+  }
+
+  const response = await sheets.spreadsheets.batchUpdate({
+    spreadsheetId,
+    resource,
+  })
+  console.log('Chart created with Id: ' + response.data.replies[0].addChart.chart.chartId);
 }
 
 async function updateCellFormatToDate(sheets, spreadsheetId, githubLastRowIndex) {
-    const formResponsesSheetId = await getSheetId(sheets, spreadsheetId, 0);
-    const githubSheetId = await getSheetId(sheets, spreadsheetId, 1);
+  const formResponsesSheetId = await getSheetId(sheets, spreadsheetId, 0);
+  const githubSheetId = await getSheetId(sheets, spreadsheetId, 1);
 
-    const requests = [{
-        repeatCell: {
-            range: {
-                sheetId: formResponsesSheetId,
-                startRowIndex: 0,
-                endRowIndex: githubLastRowIndex,
-                startColumnIndex: 0,
-                endColumnIndex: 1
-            },
-            cell: {
-                userEnteredFormat: {
-                    numberFormat: {
-                        type: 'DATE'
-                    }
-                }
-            },
-            fields: 'userEnteredFormat(numberFormat)'
-        },
-        repeatCell: {
-            range: {
-                sheetId: githubSheetId,
-                startRowIndex: 0,
-                endRowIndex: githubLastRowIndex,
-                startColumnIndex: 0,
-                endColumnIndex: 1
-            },
-            cell: {
-                userEnteredFormat: {
-                    numberFormat: {
-                        type: 'DATE'
-                    }
-                }
-            },
-            fields: 'userEnteredFormat(numberFormat)'
-        },
+  const requests = [{
+    repeatCell: {
+      range: {
+        sheetId: formResponsesSheetId,
+        startRowIndex: 0,
+        endRowIndex: githubLastRowIndex,
+        startColumnIndex: 0,
+        endColumnIndex: 1
+      },
+      cell: {
+        userEnteredFormat: {
+          numberFormat: {
+            type: 'DATE'
+          }
+        }
+      },
+      fields: 'userEnteredFormat(numberFormat)'
+    },
+    repeatCell: {
+      range: {
+        sheetId: githubSheetId,
+        startRowIndex: 0,
+        endRowIndex: githubLastRowIndex,
+        startColumnIndex: 0,
+        endColumnIndex: 1
+      },
+      cell: {
+        userEnteredFormat: {
+          numberFormat: {
+            type: 'DATE'
+          }
+        }
+      },
+      fields: 'userEnteredFormat(numberFormat)'
+    },
 
-    }];
-    const resource = {
-        requests,
-    }
+  }];
+  const resource = {
+    requests,
+  }
 
-    const response = await sheets.spreadsheets.batchUpdate({
-        spreadsheetId,
-        resource,
-    });
-    console.log(response);
+  const response = await sheets.spreadsheets.batchUpdate({
+    spreadsheetId,
+    resource,
+  });
+  console.log(response);
 }
 
