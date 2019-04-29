b1fd212af0ac9c70376187cee1351cfa163be5de
asrivas <asrivastava274@gmail.com>
2019-04-29 17:36:05 -0400

    Add CORS access

--- functions/index.js
+++ functions/index.js
@@ -7,5 +7,17 @@ const sheet = require('./sheet');
  * @param {!express:Response} res HTTP response context.
  */
 exports.githubChart = (req, res) => {
+  handleCors(req, res);
   sheet.main("Test Title IO GCF").then((id) => res.status(200).send(id));
 };
+
+handleCors = (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+  res.set("Access-Control-Max-Age", "3600");
+  if (req.method == 'OPTIONS') {
+    res.status(204).send('');
+  }
+}
+ 
